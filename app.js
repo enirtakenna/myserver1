0adee807e0fc808cffdfafc93a544764a2e035f8
enirtakenna <annekatrineegsvang@gmail.com>
2019-02-14 12:09:41 +0100

    edited app.js and added Frederikke to the room!

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port=process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Hello World by enirtakenna</h1><p>Heroku is cool! </p>');
+	res.end('<h1>Hello World by enirtakenna</h1><p>Heroku is cool! Frederikke is here too!</p>');
 });
 
 server.listen(port,() => {
