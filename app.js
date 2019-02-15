cc9a453ea3782ed10309908754155d5c371dccce
enirtakenna <annekatrineegsvang@gmail.com>
2019-02-15 12:06:01 +0100

    added html file and try to readFile through app.js

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 const port=process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
+	fs.readFile('./public/index.html', null, function(error, data){
+		if (error){
+			response.writeHead(404);
+			response.write('File not found!');
+		}
+		else {
+			response.write(data);
+		}
+		response.end();
+	});
 	res.end('<h1>Hello World by enirtakenna</h1><p>Heroku is cool! Frederikke is here too!</p>');
 });
 
 server.listen(port,() => {
 	console.log('Server running at port '+port);
 });
-
 
---
+++ task-creator.js
@@ -0,0 +1,27 @@
+
+// Task object
+/*var task = {
+    taskText: function() {
+        return document.getElementById('textInput').textContent;
+    }
+    //,
+    //date: '',
+    //color: '',
+    //tag: ''
+};*/
+
+
+/*
+Add a task when button 'add' is clicked
+addEventListener(on.click('add-btn'))
+{
+   function addTask() {
+       var task = task;
+       var makeTask = new task();
+        var addNewTask;
+
+        if () {
+           addNewTask;
+        }
+    }
+}*/
